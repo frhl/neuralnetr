a7907b5edf97278b1b1d027f92ae30e185b62fc6
frhl <frederik.heymann@gmail.com>
2020-06-11 10:37:40 -0400

    fixed collate

--- tests/testthat/test-Sequential.R
+++ tests/testthat/test-Sequential.R
@@ -1,30 +1,28 @@
 context('Sequential')
 
 
-#expect_that('Sequential can be setup like a neural network',{
+test_that('Sequential can be setup like a neural network',{
 
+  # setup problem
+  nn = Sequential$new(list(
+    Linear$new(2,5), ReLU$new(),
+    Linear$new(5,5), Tanh$new(),
+    Linear$new(5,2), SoftMax$new()),
+    NLL$new())
 
-# setup problem
-nn = Sequential$new(list(
-  Linear$new(2,10), ReLU$new(),
-  Linear$new(10,10), Tanh$new(),
-  Linear$new(10,2), SoftMax$new()),
-  NLL$new())
+  # xor problem
+  data(xor_example)
+  X = xor_example$X
+  Y = xor_example$Y
 
+  # training
+  nn$sgd(X, Y, 2500, 0.05)
 
-# xor problem
-X = t(as.matrix(data.frame(x1=c(1,2,1,2),x2=c(1,2,2,1))))
-Y = t(as.matrix(data.frame(y1=c(0,0,1,1),y2=c(1,1,0,0))))
+  # expect
+  expect_equal(nn$classify(X), c(1,1,0,0))
 
-q = nn$sgd(X, Y, 2500, 0.05)
+})
 
-
-#xor <- list(X, Y)
-#names(xor) <- c('X', 'Y')
-#saveRDS(xor, file = 'data/xor.rsa', compress = 'xz')
-
-
-plot_tidy_2d(nn, X)
 
 
 
