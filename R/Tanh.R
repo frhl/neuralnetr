e6f441d7dfbd645060c36d07087955e8ac784a7e
frhl <frederik.heymann@gmail.com>
2020-07-17 19:43:34 -0400

    added remaining code for Tanh

--- R/Tanh.R
+++ R/Tanh.R
@@ -2,7 +2,19 @@
 Tanh <- R6Class("Tanh", inherit = ClassModule, list(
 
   #' @field A the activation vector
-  A = NULL
+  A = NULL,
 
-  )
+  #' @param Z a vector of pre-activations
+  #' @return a vector of activations.
+  forward = function(Z) {
+    self$A = base::tanh(Z)
+    return(self$A)
+  },
+
+  #' @param dLdA vector of gradients.
+  #' @return a vector gradients.
+  backward = function(dLdA){
+    dLdZ = dLdA*(1 - (self$A^2))
+    return(dLdZ)
+  })
 )
